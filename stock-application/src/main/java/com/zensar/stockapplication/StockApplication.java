7d8861016c010b055986a037a909bedf9949e7ae
mobeen <m.mobeen@zensar.com>
2022-05-09 16:53:43 +0530

    hello added

--- stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
+++ stock-application/src/main/java/com/zensar/stockapplication/StockApplication.java
@@ -21,6 +21,8 @@ public class StockApplication extends SpringBootServletInitializer{
 //		s.setMarketName("nft");
 //		System.out.println(s.getMarketName());
 		
+		System.out.println("Hello");
+		
 	}
 
 	@Override
